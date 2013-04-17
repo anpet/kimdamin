3900078c701f00f3e236ab3d3e4ed60883c8e4d9
daminkim <anpet@snuev.com>
2013-04-17 19:54:58 +0900

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Damin::Application.config.secret_token = 'a7a62072298f507033b48b827b5ff7ddf972fd02fede43e5c0f3b934ebd08fe73e1bcb9985a7c2f1b42196f36c4dc3cfac984388dcd9f3c1d25edb6cccdc08c9'
